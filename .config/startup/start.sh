d153d73baefd40e5b2e5f7f1bf7e37fb5170428a
stefan911 <danielstefan.halasz@gmail.com>
2022-02-13 13:49:42 +0200

    Fixed mouse sens

--- .config/startup/start.sh
+++ .config/startup/start.sh
@@ -3,7 +3,7 @@
 /home/stefan911/Wallpapers/./setwallpaper.sh
 
 # Set mouse sens
-xinput --set-prop 11 'libinput Accel Speed' -0.8
+xinput --set-prop $(xinput | grep "Logitech G502" | grep -Eo "id=[[:digit:]]+" | head -n1 | cut -f2 -d=) 'libinput Accel Speed' -0.87
 
 # Start KeepassXC in systemtray
 keepassxc &
